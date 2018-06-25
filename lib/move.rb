cf093317705e36ee0e648f411f9239d0066d9076
Nick Graffagna <n.graffagna@gmail.com>
2018-06-25 18:49:09 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -5,7 +5,17 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
+def input_to_index("1","2","3","4", "5","6","7","8","9")
+  "1".to_ 0
+  "2".to_ 1
+  "3".to_ 2
+  "4".to_ 3
+  "5".to_ 4
+  "6".to_ 5
+  "7".to_ 6
+  "8".to_ 7
+  "9".to_ 8
+end
 board= [" "," "," "," "," "," "," "," "," "]
 def update_array_at_with(array, index, value)
   array[index] = value
