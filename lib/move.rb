aaf774cce032ba760a863e7c8d23ff92e93378dd
Nick Graffagna <n.graffagna@gmail.com>
2018-06-25 18:54:04 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -5,7 +5,7 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-def input_to_index("1","2","3","4", "5","6","7","8","9")
+def input_to_index("1","2","3","4","5","6","7","8","9")
   "1".to_ 0
   "2".to_ 1
   "3".to_ 2
